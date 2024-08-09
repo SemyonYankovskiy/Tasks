2fcea82c75734f4a9ac8953376da47d425a9414a
syankovskiy <semyon21@mail.ru>
2024-08-10 01:56:40 +0300

    pre-build Добавлены переходы по дочерним объектам

--- tasks/views.py
+++ tasks/views.py
@@ -27,10 +27,7 @@ def get_home(request):
     return render(request, "home.html", context=context)
 
 
-# def get_object_page(request, object_id):
-#     object = get_object_or_404(Object, pk=object_id)
-#     context = {"object": object}
-#     return render(request, "object-page.html", context=context)
+
 def get_object_page(request, object_id):
     object = get_object_or_404(Object, pk=object_id)
 
@@ -40,11 +37,42 @@ def get_object_page(request, object_id):
     done_count = sum(1 for task in tasks if task.is_done is True)
     not_done_count = sum(1 for task in tasks if task.is_done is False)
 
+    # Получаем дочерние объекты
+    child_objects = Object.objects.filter(parent=object)
+
+    # Исключаем первый объект из списка дочерних объектов
+    if child_objects.exists():
+        child_objects_excluding_first = child_objects[1:]
+    else:
+        child_objects_excluding_first = child_objects
+
+    # Подготовка данных дочерних объектов
+    child_data = []
+    for child in child_objects:
+        attached_files = child.files.all()
+        img_preview = attached_files.filter(file__regex=r'\.(jpeg|jpg|png)$').first()
+
+        desc = child.description[:50] + "..." if len(child.description) > 50 else child.description
+
+        child_data.append({
+            "id": child.pk,
+            "title": child.name,
+            "desc": desc,
+            "priority": child.priority,
+            "img_preview": img_preview.file.url if img_preview else None,
+        })
+
+
     context = {
         "object": object,
         "task_count": task_count,
         "done_count": done_count,
         "not_done_count": not_done_count,
+        "child_objects": child_objects_excluding_first,  # Добавляем исключённые объекты в контекст
+        "child_data": child_data,
     }
 
+
     return render(request, "object-page.html", context=context)
+
+
