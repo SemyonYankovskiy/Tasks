40c81ba6882ff89906ee4d22222ee36b0ebc815d
syankovskiy <semyon21@mail.ru>
2024-11-01 20:46:51 +0300

    dubug

--- djangoProject/extra_processors.py
+++ djangoProject/extra_processors.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 def get_random_icon(request):
     # Папка с иконками
-    icon_directory = settings.BASE_DIR/ "static/self/img/lazy"
+    icon_directory = settings.BASE_DIR / "static/self/img/lazy"
 
     # Получаем список файлов в папке
     icon_pull = list(icon_directory.rglob("*.png"))
@@ -13,8 +13,8 @@ def get_random_icon(request):
     # Если файлы есть, выбираем случайную иконку
     icon = random.choice(icon_pull) if icon_pull else None
 
-    # Формируем путь для шаблона
-    icon_path = f"self/img/lazy/{icon}" if icon else None
+    # Сохраняем только относительный путь
+    icon_path = f"self/img/lazy/{icon.name}" if icon else None
 
     return {"header_icon": icon_path}
 
